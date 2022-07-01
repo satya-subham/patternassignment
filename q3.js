550556ae169f35063d8d2cb06ca8aefcfc824ddc
satya-subham <satyasubham85@gmail.com>
2022-07-01 18:21:52 +0530

    pattern

---
+++ q3.js
@@ -0,0 +1,12 @@
+let num = 5;
+let count = 1;
+for(let i = 1; i<=5; i++){
+  stars =``
+  for(let j = 1; j<=2*i-j; j++){
+    if(i==j||j==i-j){
+     
+    } stars += count++
+    stars +="  "
+   
+  }console.log(stars)
+}
